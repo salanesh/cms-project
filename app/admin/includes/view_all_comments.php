d060ef6891d4feebcf5e88ae5c1643ec002132ef
salanesh <jpls9400@gmail.com>
2022-11-05 11:21:01 +0800

    displaying comments based on approval

--- app/admin/includes/view_all_comments.php
+++ app/admin/includes/view_all_comments.php
@@ -10,24 +10,24 @@
             <th>Date</th>
             <th>Approve</th>
             <th>UnApprove</th>
-            <th>Edit</th>
             <th>Delete</th>
         </tr>
     </thead>
     <tbody>
         <?php
-        $stmt = $connection->prepare("SELECT * FROM comments");
+        $stmt = $connection->prepare("SELECT comments.*,posts.post_title FROM comments join posts WHERE comments.comment_post_id = posts.post_id;");
         $stmt->execute();
         $result = $stmt->get_result();
         while ($row = $result->fetch_assoc()) {
             $comment_id = $row["comment_id"];
-            // $comment_post_id = $row["comment_post_id"];
+            $comment_post_id = $row["comment_post_id"];
             $comment_author = $row["comment_author"];
             $comment_email = $row["comment_email"];
             $comment_content = $row["comment_content"];
             $comment_status = $row["comment_status"];
             $comment_date = $row["comment_date"];
-            $comment_response = "someone";
+            $post_title = $row["post_title"];
+            $comment_response = "<a href='../post.php?p_id={$comment_post_id}'>$post_title</a>";
 
             echo "<tr>";
             echo "<td>{$comment_id}</td>";
@@ -38,9 +38,8 @@
             echo "<td>{$comment_status}</td>";
             echo "<td>{$comment_response}</td>";
             echo "<td>{$comment_date}</td>";
-            echo "<td><a href''>Approve</a></td>";
-            echo "<td><a href''>UnApprove</a></td>";
-            echo "<td><a href='comments.php?source=edit_comment&p_id={$comment_id}'>Edit</a></td>";
+            echo "<td><a href='comments.php?approve={$comment_id}'>Approve</a></td>";
+            echo "<td><a href='comments.php?unapprove={$comment_id}'>UnApprove</a></td>";
             echo "<td><a href='comments.php?delete={$comment_id}'>Delete</a></td>";
             echo "</tr>";
         }
@@ -49,11 +48,22 @@
     </tbody>
 </table>
 <?php
+if (isset($_GET['approve'])) {
+    $query = "UPDATE comments set comment_status='approved' WHERE comment_id = {$_GET['approve']}";
+    mysqli_query($connection, $query);
+    header("Location: comments.php");
+}
+if (isset($_GET['unapprove'])) {
+    $query = "UPDATE comments set comment_status='unapproved' WHERE comment_id = {$_GET['unapprove']}";
+    mysqli_query($connection, $query);
+    header("Location: comments.php");
+}
 if (isset($_GET['delete'])) {
-    $delete_post_id = $_GET['delete'];
-    $stmt = $connection->prepare("DELETE FROM posts WHERE post_id=?");
-    $stmt->bind_param("i", $delete_post_id);
+    $delete_comment_id = $_GET['delete'];
+    $stmt = $connection->prepare("DELETE FROM comments WHERE comment_id=?");
+    $stmt->bind_param("i", $delete_comment_id);
     $stmt->execute();
     $stmt->close();
+    header("Location: comments.php");
 }
 ?>
